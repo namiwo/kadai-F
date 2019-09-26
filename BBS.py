8c01ec51e225f6cd1bf24f6270da8632d4fdf3fb
namiwo <nhsantasan@gmail.com>
2019-09-26 11:37:31 +0900

    [課題F-2]掲示板の実装（未完）
    - デフォルト名の実装はまだ
    - 値の保持方法に迷い、別のhtmlファイルに書き込みiframeで表示させようとするなどしたが、結局csvに書き込み、リスト化した
    - 外部ファイルがあるのでサーバが落ちても保持できるのでは、と思ったが、サーバが落ちることなどそんなにないような気がするので、外部ファイルは不要だったかもしれない

---
+++ BBS.py
@@ -0,0 +1,28 @@
+import csv
+
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    if request.method == "GET":
+        return render_template("index.html")
+    if request.method == "POST":
+        username = request.form["username"]
+        message = request.form["message"]
+        list = [username, message]
+        with open("message.csv", "a") as f:
+            writer = csv.writer(f, lineterminator='\n')
+            writer.writerow(list)
+        with open("message.csv", "r") as f:
+            reader = csv.reader(f)
+            list_body = []
+            for row in reader:
+                list_body.append(row)
+        return render_template("index.html", list=list_body)
+
+
+if __name__ == '__main__':
+    app.run(debug=True, port=8888)
